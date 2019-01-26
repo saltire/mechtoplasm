f4245ea187223f9ea87b163c76bfc1df3fd35c46
saltire sable <saltiresable@gmail.com>
2019-01-25 21:41:09 -0500

    Draw a 10x10 grid of cubes

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour {
+    public GameObject cubePrefab;
+    public int gridWidth = 10;
+    public int gridHeight = 10;
+
+    void Start() {
+        for (int x = 0; x < gridWidth; x++) {
+            for (int z = 0; z < gridHeight; z++) {
+                Instantiate(cubePrefab, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+    void Update() {
+        
+    }
+}
