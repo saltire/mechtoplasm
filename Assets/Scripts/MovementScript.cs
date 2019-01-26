47764c054026b96186000edf3eaf788c052748b2
saltire sable <saltiresable@gmail.com>
2019-01-25 23:36:47 -0500

    Fire some projectiles

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+struct Fireball {
+    public GameObject obj;
+    public Vector3 target;
+}
+
 public class MovementScript : MonoBehaviour {
     public float moveSpeed = .5f;
+
+    public GameObject fireballPrefab;
+    public int fireballCount = 3;
+    public float fireTime = .5f;
+    public float fireHeight = .3f;
+    Fireball[] fireballs;
+    float fireTimeRemaining = 0;
 
     Vector3 targetPosition;
     Vector3 moveVelocity;
@@ -22,26 +35,61 @@ public class MovementScript : MonoBehaviour {
             moveVelocity = Vector3.zero;
         }
         else {
-            if (Input.GetAxisRaw("Vertical") == 1) {
-                transform.rotation = Quaternion.Euler(0, 0, 0);
-                Move();
+            GetInput();
+        }
+
+        if (fireTimeRemaining > 0) {
+            fireTimeRemaining -= Time.deltaTime;
+            float normalizedFireTime = 1 - (fireTimeRemaining / fireTime);
+
+
+            if (fireTimeRemaining <= 0) {
+                foreach (Fireball fireball in fireballs) {
+                    Destroy(fireball.obj);
+                }
             }
-            else if (Input.GetAxisRaw("Vertical") == -1) {
-                transform.rotation = Quaternion.Euler(0, 180, 0);
-                Move();
+            else {
+                foreach (Fireball fireball in fireballs) {
+                    fireball.obj.transform.position = Vector3.Lerp(transform.position, fireball.target, normalizedFireTime) + new Vector3(0, fireHeight * Mathf.Sin(normalizedFireTime * Mathf.PI), 0);
+                }
             }
-            else if (Input.GetAxisRaw("Horizontal") == 1) {
-                transform.rotation = Quaternion.Euler(0, 90, 0);
-                Move();
-            }
-            else if (Input.GetAxisRaw("Horizontal") == -1) {
-                transform.rotation = Quaternion.Euler(0, 270, 0);
-                Move();
-            }
+        }
+    }
+
+    void GetInput() {
+        if (Input.GetAxisRaw("Vertical") == 1) {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            Move();
+        }
+        else if (Input.GetAxisRaw("Vertical") == -1) {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            Move();
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 1) {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            Move();
+        }
+        else if (Input.GetAxisRaw("Horizontal") == -1) {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            Move();
+        }
+        else if (Input.GetButtonDown("Fire1") && fireTimeRemaining <= 0) {
+            Fire();
         }
     }
 
     void Move() {
         targetPosition = transform.position + transform.rotation * Vector3.forward;
+    }
+
+    void Fire() {
+        fireballs = new Fireball[fireballCount];
+        for (int i = 0; i < fireballCount; i++) {
+            fireballs[i] = new Fireball() {
+                obj = Instantiate(fireballPrefab, transform.position, Quaternion.identity),
+                target = transform.position - new Vector3(0, .6f, 0) + transform.rotation * Vector3.forward * (i + 1),
+            };
+        }
+        fireTimeRemaining = fireTime;
     }
 }
