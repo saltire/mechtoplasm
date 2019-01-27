3c20b2e438065d252a0716abde56d6d062ac4f4d
Jack <edenfall.jack@gmail.com>
2019-01-26 20:31:45 -0500

    Default weapon can destroy buildings

--- Assets/Scripts/DefaultWeaponScript.cs
+++ Assets/Scripts/DefaultWeaponScript.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DefaultWeaponScript : WeaponScript {
+
+    LayerMask buildingLayerMask;
+
     void Start() {
         Vector3 target = transform.position + transform.forward;
         Vector3Int targetCoords = player.GetCoords(target);
@@ -12,7 +15,14 @@ public class DefaultWeaponScript : WeaponScript {
             Vector3 otherPlayerTarget = transform.position + transform.forward * 2;
             player.otherPlayer.Move(otherPlayerTarget);
         }
-        
+
+        buildingLayerMask = LayerMask.GetMask("Buildings");
+
+        Collider[] buildings = Physics.OverlapSphere(target, .25f, buildingLayerMask);
+        if (buildings.Length > 0) {
+            Destroy(buildings[0].gameObject);
+        }
+
         Destroy(gameObject, 1);
     }
 }
