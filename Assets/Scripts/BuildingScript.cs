d9701e1c52b9eb9f0b9c969900b13fcc270ec590
saltire sable <saltiresable@gmail.com>
2019-01-27 14:23:54 -0500

    Add rubble

---
+++ Assets/Scripts/BuildingScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingScript : MonoBehaviour {
+    public GameObject rubblePrefab;
+
+    bool isQuitting = false;
+
+    void OnApplicationQuit() {
+        isQuitting = true;
+    }
+
+    void OnDestroy() {
+        if (!isQuitting) {
+            Instantiate(rubblePrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
