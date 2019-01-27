74f4209ca9db07cc86b279218523f5d7f5abdfd8
saltire sable <saltiresable@gmail.com>
2019-01-27 14:51:59 -0500

    Add alt rubble

--- Assets/Scripts/BuildingScript.cs
+++ Assets/Scripts/BuildingScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BuildingScript : MonoBehaviour {
     public GameObject rubblePrefab;
+    public Sprite[] rubbleSprites;
 
     bool isQuitting = false;
 
@@ -14,6 +15,7 @@ public class BuildingScript : MonoBehaviour {
     void OnDestroy() {
         if (!isQuitting) {
             GameObject rubble = Instantiate(rubblePrefab, transform.position, Quaternion.identity);
+            rubble.GetComponentInChildren<SpriteRenderer>().sprite = rubbleSprites[Random.Range(0, rubbleSprites.Length)];
             rubble.GetComponentInChildren<SpriteRenderer>().color = GetComponentInChildren<SpriteRenderer>().color;
         }
     }
