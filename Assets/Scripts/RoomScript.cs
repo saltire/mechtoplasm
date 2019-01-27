8ce78c0e4141fde352b72bca54049eac3956c0c4
Jack <edenfall.jack@gmail.com>
2019-01-27 14:49:21 -0500

    Add title and credits screen and music

---
+++ Assets/Scripts/RoomScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomScript : MonoBehaviour
+{
+ 
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("P1Fire0") || Input.GetButtonDown("P2Fire0")) {
+            SceneManager.LoadScene("Scene");
+        }
+        if (Input.GetButtonDown("P1Fire2") || Input.GetButtonDown("P2Fire2")) {
+            SceneManager.LoadScene("Credits Scene");
+        }
+        if (Input.GetButtonDown("P1Fire1") || Input.GetButtonDown("P2Fire1")) {
+            Application.Quit();
+        }
+    }
+}
