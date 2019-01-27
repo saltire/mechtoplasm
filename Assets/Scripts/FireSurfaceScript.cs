94850a506a135e344354d3788d6803184b3efae6
saltire sable <saltiresable@gmail.com>
2019-01-27 12:03:52 -0500

    Fade out surface color

--- Assets/Scripts/FireSurfaceScript.cs
+++ Assets/Scripts/FireSurfaceScript.cs
@@ -3,31 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireSurfaceScript : SurfaceScript {
-    public float duration = 4f;
-    float timeRemaining = 0;
-
     public int damage = 1;
 
-    public Color color = Color.red;
     public Color burntColor = Color.black;
-
-    void Start() {
-        square.cube.GetComponentInChildren<SpriteRenderer>().color = color;
-        ParticleSystem ps = GetComponent<ParticleSystem>();
-        ParticleSystem.MainModule psMain = ps.main;
-        psMain.duration = duration;
-        ps.Play();
-
-        timeRemaining = duration;
-    }
-
-    void Update() {
-        timeRemaining -= Time.deltaTime;
-        if (timeRemaining <= 0) {
-            square.cube.GetComponentInChildren<SpriteRenderer>().color = square.color;
-            Destroy(gameObject);
-        }
-    }
 
     public override void OnStep(PlayerScript player) {
         player.GetComponentInChildren<SpriteRenderer>().color = burntColor;
--- Assets/Scripts/SurfaceScript.cs
+++ Assets/Scripts/SurfaceScript.cs
@@ -3,7 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class SurfaceScript : MonoBehaviour {
+    public float duration = 4f;
+    float timeRemaining = 0;
+    public Color color = Color.green;
+
     public Square square;
+
+    void Start() {
+        square.cube.GetComponentInChildren<SpriteRenderer>().color = color;
+
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        if (ps != null) {
+            ParticleSystem.MainModule psMain = ps.main;
+            psMain.duration = duration;
+            ps.Play();
+        }
+
+        timeRemaining = duration;
+    }
+
+    void Update() {
+        timeRemaining -= Time.deltaTime;
+
+        square.cube.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(square.color, color, EaseOutQuint(0, 1, timeRemaining / duration));
+
+        if (timeRemaining <= 0) {
+            Destroy(gameObject);
+        }
+    }
+    
+    float EaseOutQuint(float start, float end, float value) {
+        value--;
+        end -= start;
+        return end * (value * value * value * value * value + 1) + start;
+    }
 
     public abstract void OnStep(PlayerScript player);
 }
