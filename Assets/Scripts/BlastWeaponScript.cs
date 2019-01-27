6c47405bbc77166f0b1a976bd8d2f1df7de2fcb5
saltire sable <saltiresable@gmail.com>
2019-01-27 05:59:43 -0500

    Add blue blast weapon

--- Assets/Scripts/BlastWeaponScript.cs
+++ Assets/Scripts/BlastWeaponScript.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DefaultWeaponScript : WeaponScript {
-    void Start() {
-        GetComponent<AudioSource>().Play();
+public class BlastWeaponScript : WeaponScript {
+    public int damage = 1;
 
-        Vector3 target = transform.position + transform.forward;
+    void Start() {
+        Blast(transform.forward);
+        Blast(-transform.forward);
+        Blast(transform.right);
+        Blast(-transform.right);
+
+        Destroy(gameObject, 1);
+    }
+
+    void Blast(Vector3 direction) {
+        Vector3 target = transform.position + direction;
         Vector3Int targetCoords = player.GetCoords(target);
         Vector3Int otherPlayerCoords = player.otherPlayer.GetCoords();
 
         if (otherPlayerCoords == targetCoords) {
-            Vector3 otherPlayerTarget = transform.position + transform.forward * 2;
+            Vector3 otherPlayerTarget = transform.position + direction * 2;
             player.otherPlayer.Move(otherPlayerTarget);
+            player.otherPlayer.Damage(damage);
         }
 
         LayerMask buildingLayerMask = LayerMask.GetMask("Buildings");
@@ -21,7 +31,5 @@ public class DefaultWeaponScript : WeaponScript {
         if (buildings.Length > 0) {
             Destroy(buildings[0].gameObject);
         }
-
-        Destroy(gameObject, 1);
     }
 }
